d7f10406da9989616ed802254abc4a0199f7191e
Bert van der Weerd <bert@stanzabird.nl>
2022-09-25 23:42:01 +0200

    fixing mar artifact name

--- linux-mar/assets/artifacts.mk
+++ linux-mar/assets/artifacts.mk
@@ -6,6 +6,7 @@ source_release:=$(shell cat source_release)
 full_version:=$(version)-$(source_release)$(shell [ $(release) -gt 1 ] && echo "-$(release)")
 mozbuild=~/.mozbuild
 
-incoming_artifact=librewolf-$(full_version).en-US.win64.zip
+incoming_artifact=librewolf-$(full_version).en-US.win64.installer.exe
 
 artifacts :
+	du -hs $(incoming_artifact)
