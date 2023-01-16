4c20e737115dc8e1c84bd63bcba6d35a45516a1d
Bert van der Weerd <bert@stanzabird.nl>
2023-01-16 11:57:55 +0100

    Mon Jan 16 11:57:55 AM CET 2023

--- linux/assets/artifacts.mk
+++ linux/assets/artifacts.mk
@@ -18,6 +18,7 @@ artifacts :
 	mv work/firefox work/librewolf
 	mv work/librewolf/firefox.exe work/librewolf/librewolf.exe
 	cp assets/librewolf.ico work/librewolf
+
 # this section makes the setup.exe
 	mkdir work/x86-ansi
 	wget -q -O ./work/x86-ansi/nsProcess.dll "https://shorsh.de/upload/2y9p/nsProcess.dll"
@@ -28,17 +29,22 @@ artifacts :
 	( cd work && $(mozbuild)/nsis/bin/makensis -V1 tmp.nsi )
 	rm -rf work/tmp.nsi work/librewolf.ico work/banner.bmp work/x86-ansi vc_redist.x64.exe
 	mv work/$(setupname) .
+
 # this section makes the portable.zip
 	rm -rf work/librewolf-$(full_version)
 	mkdir -p work/librewolf-$(full_version)/Profiles/Default
 	mkdir -p work/librewolf-$(full_version)/LibreWolf
 	cp -r work/librewolf/* work/librewolf-$(full_version)/LibreWolf
 	( cd work && git clone "https://github.com/ltGuillaume/LibreWolf-Portable" )
+	( cd work && git clone "https://github.com/ltGuillaume/LibreWolf-WinUpdater" )
 	cp work/LibreWolf-Portable/LibreWolf-Portable.* work/LibreWolf-Portable/*.exe work/librewolf-$(full_version)
+	cp work/LibreWolf-WinUpdater/LibreWolf-WinUpdater.ahk work/LibreWolf-Portable/*.ico work/librewolf-$(full_version)
 	wget -q -O work/ahk.zip "https://www.autohotkey.com/download/ahk.zip"
 	( mkdir work/ahk && cd work/ahk && unzip -q ../ahk.zip )
 	( cd work/librewolf-$(full_version) && wine64 ../ahk/Compiler/Ahk2Exe.exe /in LibreWolf-Portable.ahk /icon LibreWolf-Portable.ico )
 	( cd work/librewolf-$(full_version) && rm -f LibreWolf-Portable.ahk LibreWolf-Portable.ico dejsonlz4.exe jsonlz4.exe )
+	( cd work/librewolf-$(full_version) && wine64 ../ahk/Compiler/Ahk2Exe.exe /in LibreWolf-WinUpdater.ahk /icon LibreWolf-WinUpdater.ico )
+	( cd work/librewolf-$(full_version) && rm -f LibreWolf-WinUpdater.ahk LibreWolf-WinUpdater*.ico )
 # issue #224 - Consider including msvcp140 & vcruntime140 in portable package	
 	( cd work/librewolf-$(full_version)/LibreWolf && \
 	wget -q -O ./vc_redist.x64-extracted.zip "https://gitlab.com/librewolf-community/browser/windows/uploads/7106b776dc663d985bb88eabeb4c5d7d/vc_redist.x64-extracted.zip" && \
